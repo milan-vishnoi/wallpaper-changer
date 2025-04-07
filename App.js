3fb9b9b332914104e3196f60a06be3297deb07eb
Milan Vishnoi <milan.vish10@gmail.com>
2025-04-08 00:54:58 +0530

    Added the button and text and styles on these components.

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
-  return (
+  const changeWallpaper = () => {
+    console.log('Change Wallpaper');
+    alert('Wallpaper Changed!');
+  }; 
+  
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Wallpaper Changer App!!</Text>
+      <Button style={styles.button} title="Change Wallpaper" onPress={changeWallpaper} />
     </View>
   );
 }
@@ -13,8 +18,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+
 });
