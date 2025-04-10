57e8da3b9446a16317cc4847ce14fddb9b22e2b4
Milan Vishnoi <milan.vish10@gmail.com>
2025-04-11 01:08:23 +0530

    Added code to display the current wallpaper in Preview Screen.

--- screens/PreviewScreen.js
+++ screens/PreviewScreen.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
+import { WallpaperContext } from '../context/WallpaperContext';
 
 export default function PreviewScreen() {
+    const { currentWallpaper } = useContext(WallpaperContext);
     return (
         <SafeAreaView style={styles.container}>
         <Text style={styles.header}>Preview</Text>
         <Image
-            source={{ uri: 'https://images.unsplash.com/photo-1521341562101-758553412a77?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }} // Placeholder image
+            source={{ uri: currentWallpaper }} // Placeholder image
             style={styles.image}
         />
         </SafeAreaView>
